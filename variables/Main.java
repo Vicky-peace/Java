6ff0551968d9bc239ba27d0a55069493bd8f5e16
Vicky-peace <victorpeace47@gmail.com>
2025-07-02 11:35:49 +0300

    variables in java done

---
+++ variables/Main.java
@@ -0,0 +1,22 @@
+class Hello {
+    public static void main(String a[]) {
+        System.out.println("Hello World!");
+    }
+}
+
+class Peace {
+    public static void main(String a[]) {
+        int myNum = 15;
+        System.out.println("My number is: " + myNum);
+        String name = "Peace";
+        System.out.println(name);
+    }
+}
+
+class myNum {
+    public static void main(String a[]) {
+        int myNum;
+        myNum = 15;
+        System.out.println("My number is: " + myNum);
+    }
+}
