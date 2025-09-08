3533dd712b261709cd836b706a5cb99699958855
Vicky-peace <victorpeace47@gmail.com>
2025-09-08 14:13:03 +0800

    java refresher

--- 1. fundamentals/arrays.java
+++ 1. fundamentals/arrays.java
@@ -346,6 +346,13 @@
 // }
 // }
 
-// create a function that finds the second smallest item an integer array
-// input:{5,8,3,2,6,1,4,7}
 
+class Arrays {
+    public static void main(String[] args) {
+        int[] arr = { 5, 0, 8, 0, 10 };
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
