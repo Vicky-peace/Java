c2f303b6345c93e4fe295b3d713729be99c55a30
Vicky-peace <victorpeace47@gmail.com>
2025-09-09 15:41:16 +0800

    learned comparing arrays

--- 1. fundamentals/arrays.java
+++ 1. fundamentals/arrays.java
@@ -473,3 +473,15 @@ class SearchArray {
         System.out.println(Arrays.binarySearch(numbers, -3));
     }
 }
+
+// Comparing arrays
+class CompareArrays {
+    public static void main(String[] args) {
+        int[] numbers1 = { 5, 4, 3, 2, 1, 0, -1 };
+        int[] numbers2 = { 5, 4, 3, 2, 1, 0, -1 };
+        int[] numbers3 = { 1, 2, 3, 7, 7, 8, 1 };
+
+        System.out.println(numbers1 == numbers2); // false
+        System.out.println(Arrays.equals(numbers1, numbers2)); // true
+    }
+}
