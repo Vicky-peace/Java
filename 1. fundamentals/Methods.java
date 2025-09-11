e76666ad695185dcf3edb285b3670086dfdf6b92
Vicky-peace <victorpeace47@gmail.com>
2025-09-11 16:10:27 +0800

    Methods done

--- 1. fundamentals/Methods.java
+++ 1. fundamentals/Methods.java
@@ -55,3 +55,74 @@ class IfElse {
         checkAge(23);
     }
 }
+
+// Return Values
+class ReturnValues {
+    static int myMethod(int x) {
+        return 5 + x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(3));
+    }
+}
+
+class ReturnValues2 {
+    static int myMethod(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(5, 3));
+    }
+}
+
+class DoubleGame {
+    static int doubleGame(int x) {
+        return x * 2;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(doubleGame(i));
+        }
+    }
+}
+
+// Method Overloading - When multiple methods share the same name but have
+// different parameters
+// example without method overloading
+class MethodOverloading {
+    static int plusMethodInt(int x, int y) {
+        return x + y;
+    }
+
+    static double plusMethodDouble(double x, double y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        int myNum1 = plusMethodInt(8, 5);
+        double myNum2 = plusMethodDouble(4.3, 6.26);
+        System.out.println("int: " + myNum1);
+        System.out.println("double: " + myNum2);
+    }
+}
+
+// example with method overloading
+class MethodOverloading2 {
+    static int plusMethod(int x, int y) {
+        return x + y;
+    }
+
+    static double plusMethod(double x, double y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        int myNum1 = plusMethod(8, 5);
+        double myNum2 = plusMethod(4.3, 6.26);
+        System.out.println("int: " + myNum1);
+        System.out.println("double: " + myNum2);
+    }
+}
